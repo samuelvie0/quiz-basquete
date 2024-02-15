423ee281482432ba898f5fda7ebe4c389050171c
Samuel Vieira Gonçalves <samuelvie0@gmail.com>
2024-02-14 22:02:16 -0200

    ajuste no link

--- script.js
+++ script.js
@@ -93,32 +93,34 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
-const corretas = new set()
-const totalDePerguntas = pergunta.lenght
-const motrarTotal = document.querySelector('#acertos span')
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
 mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
-  
-for(const item of perguntas) {
-    const quizItem = template.content.cloneNode(true)
-    quizItem.querySelector('h3').textContent = item.pergunta
-      
-    for(let respostas of item.resposta) {
-        const dt = quizItem.querySelector('dl dt').cloneNode(true)
-        dt.querySelector('span').textContent = resposta 
-        dt.querySelector('input').setAttribute('name','pergunta-' + perguntas.indexOf(item))
-        dt.querySelector('input').value = item.resposta.indexOf(resposta)
-        quizItem.querySelector('dl').appendChild(dt)
-        dt.querySelector('input').onchange = (Event) => {const estaCorreta = Event.target.value == item.correta 
-       corretas.delete(item)
-       if(estaCorreta){correta.add(item)
-     }
-     mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
-    }   
-    quizItem.querySelector('dl dt').remove()
-}
-    
-    
- 
-    quiz.appendChild(quizItem)
-    
+
+// loop ou laço de repetição
+for (const item of perguntas) {
+  const quizItem = template.content.cloneNode(true)
+  quizItem.querySelector('h3').textContent = item.pergunta
+  for (let resposta of item.respostas) {
+    const dt = quizItem.querySelector('dl dt').cloneNode(true)
+    dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+    dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta
+
+      corretas.delete(item)
+      if (estaCorreta) {
+        corretas.add(item)
+      }
+
+      mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+    }
+    quizItem.querySelector('dl').appendChild(dt)
+  }
+
+  quizItem.querySelector('dl dt').remove()
+  // coloca a pergunta na tela
+  quiz.appendChild(quizItem)
 }
